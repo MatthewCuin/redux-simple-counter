81a6e8f3f0251907031be8e9f533d353b485ecff
Matthew Cuin <matthewcuin@Matthews-MacBook-Pro.local>
2019-09-07 11:16:51 -0400

    first redux

--- src/index.js
+++ src/index.js
@@ -10,3 +10,16 @@ subscribe(() => render(getState()))
 // Dispatch the "INCREMENT" action every time the +1 button is pressed
 const incrementButton = document.getElementById('increment');
 incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT" }));
+
+const decrementButton = document.getElementById('decrement');
+decrementButton.addEventListener('click', e => dispatch({ type: "DECREMENT" }));
+
+const colorSelect = document.getElementById('color');
+colorSelect.addEventListener('change', e => dispatch({type: "CHANGE_COLOR", color: e.target.value}));
+
+const numberButton = document.getElementById('numberSubmit');
+const numberField = document.getElementById('number');
+numberButton.addEventListener('click', e => {
+    const { value } = numberField;
+    dispatch({type: 'CHANGE_NUMBER', number: value});
+})
--- src/reducer.js
+++ src/reducer.js
@@ -9,5 +9,32 @@ const reducer = (state = initialState, action) => {
             value: state.value + 1
         }
     }
+    if (type === "DECREMENT")
+            return {
+                value: state.value - 1,
+                color: action.color
+            }
+    if (type === "INCREMENT5") {
+            return {
+                value: state.value + 5
+            }
+        }
+    if (type === "DECREMENT5") {
+        return {
+            value: state.value - 5
+            }
+        }
+    if (type === 'CHANGE_COLOR'){
+        return {
+            value: state.value,
+            color: action.color
+        }
+    }
+    if (type === 'CHANGE_NUMBER'){
+        return {
+            value: parseInt(action.value),
+            color: state.color
+        }
+    }
     return state
 }
